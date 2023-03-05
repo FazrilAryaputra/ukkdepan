95d45c749646fdb7eb029e635e5d8a2e56e4effa
fazril <fazrilaryaputra@gmail.com>
2023-03-05 20:19:53 +0700

    install axios for data fetching

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import store from './store'
 import ContentHeader from './components/ContentHeader.vue'
 import Content from './components/Content.vue'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 
 const app = createApp(App);
+// app.use(VueAxios, axios);
 
 
 app.component('content-header', ContentHeader)
